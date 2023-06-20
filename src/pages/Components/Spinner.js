3eb40b41e11980a40392a560df29967988d3dc26
john potter <juanpotter7@gmail.com>
2023-06-20 17:34:37 -0400

    display updates

--- src/pages/Components/Spinner.js
+++ src/pages/Components/Spinner.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 
 export default function Spinner({spinVal, getRandom, spinId}) {
     return (
-        <div className="fixed bottom-0 left-0 h-48 py-3 w-full text-center justify-center bg-gray-100 
-             px-4 rounded-md dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="py-8 text-center justify-center bg-gray-100 bg-opacity-40 flex flex-col gap-5
+            border border-neutral-400
+             px-5 rounded-md h-1/2 w-full">
           <div
-            className="p-8 lg:p-0 mb-4 text-2xl font-semibold"
+            className=" mb-4 text-2xl font-roboto"
           >
             {spinVal}
           </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -80,89 +80,16 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-7xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 lg:static lg:w-auto lg:p-4">
-          Next.js template screen
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-          >
-            commandeered by tatsuyoshi
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between pt-48">
+      <div className="relative flex place-items-center justify-center gap-4 w-full">
+        <div className='flex flex-col items-center w-1/4'>
+          <h2 className="mb-6 text-2xl font-semibold">Genre</h2>
+          <Spinner spinVal={genres[genreSpin]} getRandom={getRandomIntInclusive} spinId={0} />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center gap-4">
-        <Spinner spinVal={genres[genreSpin]} getRandom={getRandomIntInclusive} spinId={0} />
-        <Spinner spinVal={modifier[modSpin]} getRandom={getRandomIntInclusive} spinId={1} />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className='flex flex-col items-center w-1/4'>
+          <h2 className="mb-6 text-2xl font-semibold">Featuring</h2>
+          <Spinner spinVal={modifier[modSpin]} getRandom={getRandomIntInclusive} spinId={1} />
+        </div>
       </div>
     </main>
   )
